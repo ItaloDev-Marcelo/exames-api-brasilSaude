b73345ee5d7dc586d5a36a5d86b4275722a90c4f
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-03-31 14:35:47 -0300

    Feat: add connectToDb and routes

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT;
+const connectToDb = require('./database/database');
+const authRoute = require('./routes/auth')
+
+connectToDb();
+
+app.use(express.json())
+app.use('/api/auth', authRoute);
 
 app.listen(PORT, () => {
     console.log(`Server running on Port ${PORT}`)
