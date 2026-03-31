a1fffe1465309951631d12a9169793b9ab5fe007
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-03-31 15:13:49 -0300

    chore(server) add Exames Route to server.js

--- server.js
+++ server.js
@@ -4,11 +4,12 @@ const app = express();
 const PORT = process.env.PORT;
 const connectToDb = require('./database/database');
 const authRoute = require('./routes/auth')
-
+const examesRoute = require('./routes/user')
 connectToDb();
 
 app.use(express.json())
 app.use('/api/auth', authRoute);
+app.use('/api/Exames', examesRoute);
 
 app.listen(PORT, () => {
     console.log(`Server running on Port ${PORT}`)
