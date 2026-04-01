e5417007a43dbc8ec463b1020003e28f2cb60828
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-04-01 15:43:43 -0300

    refactore: add Cors to server.js

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const PORT = process.env.PORT;
 const connectToDb = require('./database/database');
 const authRoute = require('./routes/auth')
 const examesRoute = require('./routes/user')
+const cors = require('cors')
+app.use(cors())
 connectToDb();
 
 app.use(express.json())
 app.use('/api/auth', authRoute);
-app.use('/api/Exames', examesRoute);
+app.use('/api/exames', examesRoute);
 
 app.listen(PORT, () => {
     console.log(`Server running on Port ${PORT}`)
