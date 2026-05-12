6e5be0d4443eddc91d2d9a1261f81cd788463f65
Italo Marcelo <Devfrontend2028study@gmail.com>
2026-05-12 11:25:40 -0300

    style:(server) change router name user to arquivo

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT;
 const connectToDb = require('./database/database');
 const authRoute = require('./routes/auth')
-const examesRoute = require('./routes/user')
+const examesRoute = require('./routes/arquivo')
 connectToDb();
 
 app.use(express.json())
